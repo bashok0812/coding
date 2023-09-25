b48196de30be0dd75f57ce32cf9b13ceb7f2d365
bashok0812 <bashok0812@gmail.com>
2023-09-25 08:42:50 +0530

    Pick from both sides!

--- arrays/ArraySumFromEnds.java
+++ arrays/ArraySumFromEnds.java
@@ -1,3 +1,12 @@
+/**
+ * Pick from both sides!
+ * 
+ * Given an integer array A of size N.
+ * You have to pick exactly B elements from either left or right end of the array A to get the maximum sum.
+ * Find and return this maximum possible sum.
+ * NOTE: Suppose B = 4 and array A contains 10 elements then
+ * You can pick the first four elements or can pick the last four elements or can pick 1 from the front and 3 from the back etc. you need to return the maximum possible sum of elements you can pick.
+ */
 public class ArraySumFromEnds {
     public int solve(ArrayList<Integer> a, int b) {
         
